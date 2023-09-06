a13e1f07d71280b3f66a3643067b4b822c07b702
GiorgiaDiGregorio <130393650+GiorgiaDiGregorio@users.noreply.github.com>
2023-09-07 01:52:11 +0200

    .

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const carousel = document.querySelector(".carosello");
+const arrowBtns = document.querySelectorAll(".container i");
+const firstCardWidth = carousel.querySelector(".card-gradient").offsetWidth;
+
+let isDragging = false, startX, startScrollLeft;
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+    })
+})
+   
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+
+}
+
+const dragging = (e) => {
+    if(!isDragging) return;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
